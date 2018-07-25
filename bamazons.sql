1433d9bb2cd41c9194bf04fb7e7b02970fab66f3
Katri Morrison-Goulias <katrimorrison-goulias@Konstadinoss-MacBook-Air.local>
2018-07-25 13:42:48 -0700

    added 10 products

--- bamazons.sql
+++ bamazons.sql
@@ -14,4 +14,15 @@ CREATE TABLE products(
 
 SELECT * FROM products;
 
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("NewAmsterdam","Beverage",19.99,200),
+    ("Chardonnay","Beverage",5.99,100),
+    ("Socks","Clothing",7.99,50),
+    ("Cheerios","Food",3.95,20),
+    ("Oats","Food",5.99,100),
+    ("Tanks","Clothing",12.05,20),
+    ("Watermelon","Food",2.99,5),
+    ("Mango Juice","Beverage",6.99,30),
+    ("Ring Pop","Food",1.99,10),
+    ("Shorts","Clothing",27.19,29);
 
